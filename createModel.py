bd7805dd878c4c563beabcb8956350be4d61a2cb
aloosen92 <alexander.loosen@gmx.de>
2022-03-18 17:14:05 +0100

    no message

--- createModel.py
+++ createModel.py
@@ -12,6 +12,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.layers import SpatialDropout1D
+from keras import optimizers
 
 import numpy as np
 
@@ -34,13 +35,17 @@ def createClassifier(classType, Xtrain, ytrain):
 
 def createLSTM(Xtrain):
     model = Sequential()
-    model.add(LSTM(units=1, return_sequences=True, input_shape = (Xtrain.shape[1],Xtrain.shape[2]), activation='relu'))
+    model.add(LSTM(units=5, return_sequences=True, input_shape = (Xtrain.shape[1],Xtrain.shape[2]), activation='sigmoid'))
     model.add(Dropout(0.2))
-    model.add(LSTM(units=1, return_sequences=True, input_shape = (Xtrain.shape[1],Xtrain.shape[2]), activation='relu'))
-    model.add(Dropout(0.2))
+#    model.add(LSTM(units=1, return_sequences=True, input_shape = (Xtrain.shape[1],Xtrain.shape[2]), activation='relu'))
+#    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='relu'))
 
-    #opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics='accuracy')
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics='accuracy')
+    #model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics='accuracy')
+    #sgd = tf.keras.optimizers.SGD(learning_rate=0.01, decay=1e-4, momentum=0.7, nesterov=True)
+    #model.compile(loss='mean_squared_error', optimizer=sgd, metrics='accuracy')
     model.build()
     
     return model
@@ -48,12 +53,14 @@ def createLSTM(Xtrain):
 def createNN(Xtrain):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(Xtrain.shape[1], input_dim=Xtrain.shape[1], activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(round(Xtrain.shape[1]/2), activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(round(Xtrain.shape[1]/3), activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid))
+    model.add(tf.keras.layers.Dense(Xtrain.shape[1], input_dim=Xtrain.shape[1], activation='relu'))#, activation=tf.nn.relu))
+    model.add(Dropout(0.2))
+    #model.add(tf.keras.layers.Dense(round(Xtrain.shape[1]/2), activation='relu'))#, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(round(Xtrain.shape[1]/3), activation='relu'))#, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(1, activation='relu'))
 
-    model.compile(optimizer='adam',
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-5)#, decay=1e-5)
+    model.compile(optimizer=opt,
                  loss='binary_crossentropy',
                  metrics=['accuracy'])
 #    model.build()
